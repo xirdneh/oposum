b9996f6786113edecca86ed48a6f6f0a9da19b62
Josue Balandrano Coronel <xirdneh@gmail.com>
2015-08-06 07:33:21 -0500

    Docker compose created

--- oPOSum/settings/common.py
+++ oPOSum/settings/common.py
@@ -47,12 +47,12 @@ STATIC_URL = '/static/'
 LOGIN_URL = '/login/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (
+#STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     #'/home/xirdneh/projects/django/oPOSum/oposum_static/',
-)
+#)
 
 # List of finder classes that know how to find static files in
 # various locations.
